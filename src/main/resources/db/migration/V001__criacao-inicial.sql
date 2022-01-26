bf2cb4b67d7f0aa0325b4492a37eca1b73fa4395
dionlan <dionlan.alves@gmail.com>
2022-01-26 14:46:42 -0300

    Bug fix: refatoração para não deletar os dados após a inicialização do spring boot.

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,21 +1,4 @@
-create table book (
-	id bigint not null auto_increment,
-    author varchar(255) not null,
-    launch_date datetime not null,
-    price decimal(10,2) not null,
-    title varchar(255) not null,
-    
-    primary key(id) 
-) engine=InnoDB default charset=utf8;
-
-create table permission (
-	id bigint not null auto_increment,
-    description varchar(255) not null,
-    
-    primary key(id) 
-) engine=InnoDB default charset=utf8;
-
-create table person (
+create table IF NOT EXISTS person (
 	id bigint not null auto_increment,
     first_name varchar(80) not null,
     last_name varchar(80) not null,
@@ -25,15 +8,46 @@ create table person (
     primary key(id) 
 ) engine=InnoDB default charset=utf8;
 
-create table user (
+create table IF NOT EXISTS book (
 	id bigint not null auto_increment,
-    user_name varchar(80) not null,
-    full_name varchar(80) not null,
-    password varchar(80) not null,
-    account_non_expired boolean not null,
-    account_non_locked boolean not null,
-    credentials_non_expired boolean not null,
-    enabled boolean not null,
+    author varchar(255) not null,
+    launch_date datetime not null,
+    price decimal(10,2) not null,
+    title varchar(255) not null,
     
     primary key(id) 
 ) engine=InnoDB default charset=utf8;
+
+create table IF NOT EXISTS permission (
+	id bigint not null auto_increment,
+    description varchar(255) not null,
+    
+    primary key(id) 
+) engine=InnoDB default charset=utf8;
+
+create table IF NOT EXISTS user (
+	id bigint not null auto_increment,
+    user_name varchar(255) not null,
+    full_name varchar(255) not null,
+    password varchar(255) not null,
+    account_non_expired boolean default null,
+    account_non_locked boolean default null,
+    credentials_non_expired boolean default null,
+    enabled boolean default null,
+    
+    primary key(id),
+    unique key uk_user_name (user_name)
+) engine=InnoDB default charset=utf8;
+
+CREATE TABLE IF NOT EXISTS user_permission (
+  id_user bigint(20) NOT NULL,
+  id_permission bigint(20) NOT NULL,
+  
+  primary key (id_user, id_permission),
+  key fk_user_permission_permission (id_permission),
+  CONSTRAINT fk_user_permission FOREIGN KEY (id_user) REFERENCES user (id),
+  CONSTRAINT fk_user_permission_permission FOREIGN KEY (id_permission) REFERENCES permission (id)
+) ENGINE=InnoDB;
+
+ALTER TABLE person
+	ADD COLUMN enabled BIT(1) NOT NULL DEFAULT b'1' AFTER last_name;
